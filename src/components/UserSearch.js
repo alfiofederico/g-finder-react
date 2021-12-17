8a83afc245dc36994c5604b8d61e910989f9451b
Alfio Federico Di Pinto <fefferico81@gmail.com>
2021-12-17 16:35:19 +0900

    refactored and fixed some bugs

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -1,11 +1,15 @@
 import { useState, useContext } from "react";
 import GithubContext from "../context/github/GithubContext";
+import AlertContext from "../context/alert/AlertContext";
+import {searchUsers} from "../context/github/GithubActions"
+
 
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { users, dispatch} = useContext(GithubContext);
   
+  const {setAlert} = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -13,10 +17,13 @@ function UserSearch() {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter something");
+      setAlert('Please type something', 'error')
     } else {
       
-      searchUsers(text)
+      dispatch({ type: "SET_LOADING"});
+      const users = await searchUsers(text)
+
+      dispatch({type:'GET_USERS', payload: users})
       setText("");
     }
   };
@@ -47,7 +54,7 @@ function UserSearch() {
       {users.length > 0 && (
         <div>
           <button
-            onClick={clearUsers}
+            onClick={() => dispatch({type: 'CLEAR_USERS'})}
             className="btn btn-ghost btn-lg"
           >
             Clear
